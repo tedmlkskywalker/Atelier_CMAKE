aaec4995f55841ef6f1d29d30c4413af2494279a
tedmlkskywalker <luffylecrack@gmail.com>
2025-12-10 09:38:30 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -32,4 +32,19 @@ int main(int argc, char *argv[]){
 	return 0;
 }
 
+else if (argc == 3)  // cas 1 argument (car)
+{
+    char* op = argv[1];
+    char* a = argv[2];
+    double r = 0;
+
+    if (strcmp(op, "car") == 0) {
+        r = _car(atof(a));
+        printf("%lf", r);
+    }
+    else {
+        printf("Erreur de parametres");
+    }
+}
+
 		
