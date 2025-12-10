a48afc9f469df83efa92a066f98f731aa7ef3112
tedmlkskywalker <luffylecrack@gmail.com>
2025-12-10 09:12:25 +0100

    Enhance calculator with 'car' operation
    Added support for a new operation 'car' with one argument.

--- src/main.c
+++ src/main.c
@@ -4,32 +4,50 @@
 #include "./../include/calculator.h"
 
 int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
-	else {printf("Erreur de parametres");}
-	return 0;
-}
+    if (argc == 4)
+    {
+        char* op = argv[1];
+        char* a = argv[2];
+        char* b = argv[3];
+        double r = 0;
 
-		
+        if (strcmp(op, "add") == 0) {
+            r = _add(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "sub") == 0) {
+            r = _sub(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "mul") == 0) {
+            r = _mul(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else if (strcmp(op, "div") == 0) {
+            r = _div(atof(a), atof(b));
+            printf("%lf", r);
+        }
+        else {
+            printf("Erreur de parametres");
+        }
+    }
+    else if (argc == 3)   // 🔵 cas spécial : operation avec 1 seul nombre
+    {
+        char* op = argv[1];
+        char* a = argv[2];
+        double r = 0;
+
+        if (strcmp(op, "car") == 0) {
+            r = _car(atof(a));   // ➜ appel de ta nouvelle fonction
+            printf("%lf", r);
+        }
+        else {
+            printf("Erreur de parametres");
+        }
+    }
+    else {
+        printf("Erreur de parametres");
+    }
+
+    return 0;
+}
